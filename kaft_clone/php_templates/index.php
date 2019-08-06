e109eac113387a8bc0590298c6631aecfbaf6e91
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2019-08-06 17:52:34 +0300

    html template files added

---
+++ kaft_clone/php_templates/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'base/components/head.html' ?>
+    <title>Document </title>
+</head>
+<body>
+<?php include 'base/components/nav.html' ?>
+    <?php include 'home/carousel.html' ?>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+
+            <div class="col-sm-4">
+            <?php include 'product/product.html' ?>
+            </div>
+            
+        </div>   
+    </div>
+    <?php include 'base/components/footer.html' ?>
+    <?php include 'base/components/footer_scripts.html' ?>
+</body>
+</html>
