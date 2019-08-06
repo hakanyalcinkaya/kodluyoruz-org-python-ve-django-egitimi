a814a43b1b34184a169c80905e3ded9495aca7fa
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2019-08-06 20:53:03 +0300

    product model added

--- kaft_clone/page/models.py
+++ kaft_clone/page/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+STATUS = [
+    ('draft', 'Taslak'),
+    ('published', 'Yayinlandi'),
+    ('deleted', 'Silindi'),
+]
+
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    # slug : sadece create sirasinda olusmalidir
+    content = models.TextField() 
+    cover_image = models.ImageField(upload_to='page')
+    status = models.CharField(
+        default="draft", 
+        choices=STATUS,
+        max_length=10,
+    )
+    createt_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
