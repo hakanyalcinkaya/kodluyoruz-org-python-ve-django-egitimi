b442892e86063da507ff603a4933b6c491e08349
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2019-08-07 20:39:16 +0300

    static media added

--- kaft_clone/kaft_clone/urls.py
+++ kaft_clone/kaft_clone/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from page.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='index'), 
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- kaft_clone/page/views.py
+++ kaft_clone/page/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Carousel
 
 def index(request):
     context = dict()
+    context['images'] = Carousel.objects.filter(status="published")
+    # context['images'] = images
     return render(request, 'home/index.html', context)
