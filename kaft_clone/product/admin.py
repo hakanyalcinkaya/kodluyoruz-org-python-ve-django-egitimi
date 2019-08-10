a16a179c4e1d562904862cff73329860a1545d7d
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2019-08-10 11:59:00 +0300

    product models added

--- kaft_clone/product/admin.py
+++ kaft_clone/product/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        'pk',
+        'title',
+        'slug',
+        'status', 
+        'updated_at',
+    )
+    list_filter = ('status', )
+    list_editable = (
+        'title',
+        'status', 
+    )
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        'pk',
+        'title',
+        'price',
+        'stock',
+        'slug',
+        'status', 
+        'updated_at',
+    )
+    list_filter = ('status', )
+    list_editable = (
+        'title',
+        'status', 
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- kaft_clone/product/models.py
+++ kaft_clone/product/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from page.models import DEFAULT_STATUS, STATUS
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(
+        max_length=200, 
+        default="",
+    )
+    status = models.CharField(
+        default=DEFAULT_STATUS, 
+        choices=STATUS,
+        max_length=10,
+    )
+    createt_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(
+        max_length=200, 
+        default="",
+    )
+    content = models.TextField() 
+    cover_image = models.ImageField(
+        upload_to='page',
+        null=True,
+        blank=True,
+    )
+    price = models.FloatField()
+    stock = models.PositiveSmallIntegerField(default=0)
+    status = models.CharField(
+        default=DEFAULT_STATUS, 
+        choices=STATUS,
+        max_length=10,
+    )
+    createt_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
