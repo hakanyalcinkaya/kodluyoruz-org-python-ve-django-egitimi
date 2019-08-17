ddfdb200297679867c04b342c65e21cb13595bf1
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2019-08-17 13:01:36 +0300

    cart model updated

--- kaft_clone/cart/models.py
+++ kaft_clone/cart/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from product.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = [
+    # left side: DB
+    # right side: human-readable name
+    ('waiting', 'Bekleniyor'),
+    ('buyed', 'Satinalindi'),
+    ('deleted', 'Silindi'),
+]
+
+
+class ShoppingCartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.FloatField()
+    is_deleted = models.BooleanField(default=False)
+    createt_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.CASCADE
+    )
+    items = models.ManyToManyField(ShoppingCartItem, blank=True)
+    total_price = models.FloatField()
+    status = models.CharField(
+        default="waiting", 
+        choices=STATUS,
+        max_length=10,
+    )
+    createt_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
